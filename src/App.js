e2953ee611d7add24e95c076e26c80fe4e3675d9
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-03-13 17:13:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+import { getD6Roll } from "./utils";
+import useLocalStorageState from "use-local-storage-state";
+
+export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+  const currentRollValue = rolls[0]?.value;
+
+  return (
+    <div className="app">
+      <main className="app__main">
+        <D6Button onRoll={handleRoll} value={currentRollValue} />
+      </main>
+      <aside className="app__aside">
+        <History rolls={rolls} />
+        <button
+          onClick={() => {
+            localStorage.clear();
+            setRolls([]);
+          }}
+        >
+          tabula rasa
+        </button>
+      </aside>
+    </div>
+  );
+}
